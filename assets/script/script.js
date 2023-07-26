fe83847921f0faa78ab2da654c9f8aa2606e8872
unknown <emmansairun@gmail.com>
2023-07-26 23:29:28 +0100

    added github file

--- assets/script/script.js
+++ assets/script/script.js
@@ -36,37 +36,28 @@ function rollBall () {
   if (count === 10) {
     score[0].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[0].innerHTML = totalHits
-    btn.style = "animation: rotation 0.1s;"
   } else if (count === 20) {
     score[1].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[1].innerHTML = totalScores
-    btn.style = "animation: rotation 0.1s;"
   } else if (count === 30) {
-    btn.style = "animation: rotation 0.1s;"
     score[2].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[2].innerHTML = totalScores
   } else if (count === 40) {
-    btn.style = "animation: rotation 0.1s;"
     score[3].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[3].innerHTML = totalScores
   } else if (count === 50) {
-    btn.style = "animation: rotation 0.1s;"
     score[4].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[4].innerHTML = totalScores
   } else if (count === 60) {
-    btn.style = "animation: rotation 0.1s;"
     score[5].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[5].innerHTML = totalScores
   } else if (count === 70) {
-    btn.style = "animation: rotation 0.1s;"
     score[6].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[6].innerHTML = totalScores
   } else if (count === 80) {
-    btn.style = "animation: rotation 0.1s;"
     score[7].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[7].innerHTML = totalScores
   } else if (count === 90) {
-    btn.style = "animation: rotation 0.1s;"
     score[8].innerHTML = `${pinsHitFirst}, ${pinsHitSecond}`
     totalScore[8].innerHTML = totalScores
   } else if (count === 100) {
